f86f4f3f2fc04b944eab8961e65ca50910d35d80
An Le Thanh <anlethanh@Ans-MacBook-Pro.local>
2021-03-06 15:15:43 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [sortType, setSortType] = useState(SortType.BUBBLE_SORT);
+  const [arr, setArr] = useState([] as number[]);
+
+  useEffect(() => {
+    generateRandomArray();
+  }, []);
+
+  const generateRandomArray = () => {
+    const arrLength = 6;
+    const res = [];
+    for (let i = 0; i < arrLength; i++) {
+      const randomNum = Math.floor(Math.random() * 10);
+      res.push(randomNum);
+    }
+
+    console.log(res);
+    setArr(res);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Algo Visualizer by An</h1>
       </header>
+      <section>
+        <button>Generate Array</button>
+
+        <select
+          value={sortType}
+          onChange={(e) => setSortType(e.target.value as SortType)}
+        >
+          <option value={SortType.BUBBLE_SORT}>Bubble Sort</option>
+          <option value={SortType.INSERTION_SORT}>Insertion Sort</option>
+        </select>
+        <button>Sort</button>
+      </section>
     </div>
   );
+}
+
+enum SortType {
+  BUBBLE_SORT = "BUBBLE_SORT",
+  INSERTION_SORT = "INSERTION_SORT",
 }
 
 export default App;
